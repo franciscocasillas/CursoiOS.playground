c6b16f95c86760d5491b4b372f759d36aa880b7f
Francisco J. Casillas <chiccasillas@gmail.com>
2024-06-25 11:14:37 -0600

    First Swift exercise

---
+++ Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Ejercicio: Registro de asistencia
+//Declara una variable asistencias y asígnale un número de asistencia a una clase
+//Declara una variable totalClases y asígale el número total de clases
+//Calcula el porcentaje de asistencia utilizando la fórmula: Porcentaje = (Asistencias / TotalClases) * 100
+//Imprime el porcentaje de asistencia
+
+var asistencias:Float = 25
+var totalClases:Float = 100
+var porcentaje:Float = (asistencias / totalClases) * 100
+print(porcentaje)
+
+
